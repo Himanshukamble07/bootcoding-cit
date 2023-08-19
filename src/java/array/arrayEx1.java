64414528cb95c920136f08bc563425fe89f39dc6
Lab_2 <himanshukamble17@gmail.com>
2023-08-19 12:08:26 +0530

    assignment array

---
+++ src/java/array/arrayEx1.java
@@ -0,0 +1,12 @@
+package array;
+
+public class arrayEx1 {
+    public static void main(String[] args) {
+        int[]temp;
+        int []arr={5,9,15,2,8,67,35,6,89};
+                int n= arr.length;
+        for (int i=0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
